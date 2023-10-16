3078222aa0b255f9f9abd42a33b1f9050596db26
LO-VE-0 <hsg151515@naver.com>
2023-10-16 15:15:25 +0900

    index header, slide

---
+++ js/slideshow.js
@@ -0,0 +1,40 @@
+var slideIndex = 1;
+
+showSlides(slideIndex);
+
+// 이전/다음 버튼 클릭 시 슬라이드 변경
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// 현재 슬라이드 표시
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+// 슬라이드 표시 함수
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// 2초마다 자동으로 슬라이드 넘기기
+setInterval(function () {
+    plusSlides(1);
+}, 5000);
+
