7e46e0e18f5e98be62cdb3ace99f9621d9d51be3
sg-hwang-333 <hsg151515@naver.com>
2024-04-08 12:13:51 +0900

    [ERROR] json is not defined

--- js/recycle_js/popup.js
+++ js/recycle_js/popup.js
@@ -1,23 +1,37 @@
-import json from '../../json/popup.json';
-
 let target = document.querySelectorAll('.btn_open');
 let popCloseBtn = document.querySelectorAll('.pop_wrap .btn_close');
+let popInfo = document.querySelector('#pop_info');
 let targetID;
 
+fetch('../../json/popup.js')
+.then(function(response) {
+return response.json();
+})
+.then(function(myJson) {
+console.log(JSON.stringify(myJson));
+});
 
 function openPopup(elememt) {
   console.log(elememt.id)
 
   switch (elememt.id) {
-    case 'origin-name': targetID = json.food[0];
+    case 'origin-name': targetID = json.food[0]; break;
   }
 
-  document.querySelector(targetID).style.display = 'block';
+  popInfo.style.display = 'block';
+  popInfo.innerHTML = targetID;
 }
 
-// 팝업 닫기
-for(let j = 0; j < target.length; j++){
-  popCloseBtn[j].addEventListener('click', function(){
-    this.parentNode.parentNode.style.display = 'none';
-  });
-}
+// for(var i = 0; i < target.length; i++){
+//   target[i].addEventListener('click', function(){
+//     targetID = this.getAttribute('href');
+//     document.querySelector(targetID).style.display = 'block';
+//   });
+// }
+
+// // 팝업 닫기
+// for(let j = 0; j < target.length; j++){
+//   popCloseBtn[j].addEventListener('click', function(){
+//     this.parentNode.parentNode.style.display = 'none';
+//   });
+// }
